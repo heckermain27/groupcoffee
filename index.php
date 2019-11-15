5ea72f4e2fd82061b8d1d95a0e27fe15e0934ab5
Cole <57687273+heckermain27@users.noreply.github.com>
2019-11-15 00:35:57 -0800

    Update index.php

--- index.php
+++ index.php
@@ -17,3 +17,28 @@
     </form>
   </body>
 </html>
+
+<?php
+    if($_POST['Submit']) {
+      print "<h1>Your Comment Has Been Submitted!</h1>";
+        
+      $Name = $_POST['Name'];
+      $Comment = $_POST['Comment'];
+      
+      # Get Old Comments
+      $old = fopen("comments.txt", "r+t");
+      $old_comments = fread($old, 1024);
+      
+      # Delete everything, write down new and old comments.
+      $write = fopen("comments.txt", "w+");
+      $string = "<b>".$Name."</b><br>".$Comment."<br>\n".$old_comments;
+      fwrite($write, $string);
+      fclose($write);
+      fclose($old);
+    }
+
+    # Read Comments
+    $read = fopen("comments.txt", "r+t");
+    echo "<br><br>Comments<hr>".fread($read, 1024);
+    fclose($read);
+?>
